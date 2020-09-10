859e2412201412bf8aff145290dea004cf94c759
Yong Hoon Shin <zcabyhs@ucl.ac.uk>
2020-09-10 09:15:25 +0800

    Add Redis to create pub/sub for each party and store/fetch users in party

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import ws from "ws";
 import express from "express";
 import http from "http";
-import { v4 as uuid } from "uuid";
+import { v4 as uuidv4 } from "uuid";
+import redis from "redis";
 
 const SERVER_ID = process.env.SERVER_ID; // for horizontally scaling with docker
 const SERVER_PORT = 8080;
-
-// const REDIS_HOST = "127.0.0.1";
-// const REDIS_PORT = 6379;
+const REDIS_HOST = "127.0.0.1";
+const REDIS_PORT = 6379;
 
 declare module "ws" {
   export class WebSocket extends ws {
@@ -15,26 +15,69 @@ declare module "ws" {
   }
 }
 
+// express + websocket server
 const app = express();
 const server = http.createServer(app);
 const wss = new ws.Server({ server });
 
+// redis
+const cli = redis.createClient(REDIS_PORT, REDIS_HOST);
+const sub = cli.duplicate();
+const pub = cli.duplicate();
+
+sub.on("message", (channel, message) => {
+  console.log(`Received message from ${channel}:  ${message}`);
+});
+
+// express endpoint
 app.get("/", (_, res) => {
   res.send("Websocket server for the movens chrome extension");
 });
 
-wss.on("connection", (user, req) => {
-  console.log("User connected from ", req.url);
+// wss handlers
+const parties = new Set<string>();
 
-  user.id = uuid();
+function createChannelAndSub(partyId: string) {
+  parties.add(partyId);
+  sub.subscribe(partyId);
+}
 
-  user.on("message", (data) => {
+function getUsersInParty(partyId: string, callback: (err: Error | null, res: string[]) => void) {
+  cli.hvals(`${partyId}:users`, (err, res) => {
+    callback(err, res);
+  });
+}
+
+class User {
+  userId: string;
+  partyId: string;
+
+  constructor(userId: string, partyId: string) {
+    this.userId = userId;
+    this.partyId = partyId;
+  }
+}
+
+wss.on("connection", (socket, req) => {
+  socket.id = uuidv4();
+  console.log(`New user ${socket.id} connected!`);
+
+  socket.send(
+    JSON.stringify({
+      type: "userId",
+      payload: {
+        userId: socket.id
+      }
+    })
+  );
+
+  socket.on("message", (data) => {
     let msg;
     try {
       msg = JSON.parse(data as string);
     } catch (error) {
       console.log("Malformed message from client");
-      user.send(
+      socket.send(
         JSON.stringify({
           type: "error",
           payload: { message: "Message sent was not in JSON format" }
@@ -42,21 +85,31 @@ wss.on("connection", (user, req) => {
       );
     }
 
-    // msg is properly formatted here
     switch (msg.type) {
-      case "join":
-        // const {partyId} = msg.payload
-        // create new channel from the party id
-        // TODO: fetch users from given party using partyId
-        user.send(
-          JSON.stringify({ type: "currentPartyUsers", payload: { users: ["user1", "user2"] } })
-        );
+      case "getCurrentPartyUsers": {
+        const { partyId } = msg.payload;
+        // 1) Subscribe to channel partyId
+        createChannelAndSub(partyId);
+        // 2) Get currently existing sockets and send
+        getUsersInParty(partyId, (err, users) => {
+          if (err) {
+            console.error("Error fetching data for users");
+          } else {
+            console.log(`There are ${users.length} users currently in party ${partyId}`);
+            socket.send(JSON.stringify({ type: "currentPartyUsers", payload: { users } }));
+          }
+        });
+        // 3) Create User instance and save it to "partyId:participants" using hset
+        const newUser = new User(socket.id, partyId);
+        cli.hset(`${partyId}:users`, socket.id, JSON.stringify(newUser));
         break;
-      default:
+      }
+      default: {
         console.log("Could not identify message type");
-        user.send(
+        socket.send(
           JSON.stringify({ type: "error", payload: { message: "Not a valid message type" } })
         );
+      }
     }
   });
 });
